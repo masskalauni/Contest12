97db1efdd8ba909ea2c7c5dff49bd7906e04cf9b
Bishnu Rawal <bishnu.rawal@verscend.com>
2018-09-05 00:17:38 +0545

    BS4 design changes

---
+++ Kolpi.Web/Models/ScoreCard/TeamAnalyticsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Kolpi.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolpi.Models.ScoreCard
+{
+    public class TeamAnalyticsViewModel
+    {
+        public int TotalTeams { get; set; }
+        public int TotalParticipants { get; set; }
+        public Dictionary<Theme, int> ThemeSelection { get; set; }
+        
+    }
+}
